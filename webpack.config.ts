bb591cbfad084ca22dc34f1eac9b1e67c8c6da88
ZhurmilovVadim <vzhurmilov@gmail.com>
2024-03-12 10:38:30 +0300

    переезд на typescript, добавил линтер, webpack, babel и подключил ScReact

---
+++ webpack.config.ts
@@ -0,0 +1,85 @@
+import path from 'path';
+import webpack from "webpack";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+import 'webpack-dev-server'
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config : webpack.Configuration = {
+    entry: path.resolve(__dirname, 'public', 'index.ts'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ['@babel/preset-typescript', { targets: "defaults" }]
+                        ],
+                        plugins: [
+                            [
+                                '@babel/plugin-transform-react-jsx',
+                                { runtime: "automatic", importSource: "/public/src/lib/scReact/" },
+                            ]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        config.plugins.push(new MiniCssExtractPlugin());
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
+
+export default config;
