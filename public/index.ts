b771e26f22b24c42a7e0d10d3135bf04ea6488c8
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-03-25 19:53:03 +0300

    Поправил service worker

--- public/index.ts
+++ public/index.ts
@@ -7,7 +7,7 @@ renderDOM('root', ScReact.createComponent(App, {}));
 
 if ('serviceWorker' in navigator) {
     // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('sw.ts')
+    navigator.serviceWorker.register('./sw.js')
         .then(() => navigator.serviceWorker.ready.then((worker) => {
             console.log("worker.sync")
             worker.sync.register('syncdata');
