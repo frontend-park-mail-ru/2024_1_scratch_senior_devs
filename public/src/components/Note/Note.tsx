9e02350318f22f1d315854f0eeeda6777728e430
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-05-06 00:29:38 +0300

    Починил баг с превьюшками

--- public/src/components/Note/Note.tsx
+++ public/src/components/Note/Note.tsx
@@ -14,17 +14,14 @@ type NoteProps = {
 
 export class Note extends ScReact.Component<NoteProps, any> {
     render() {
-
-        const isOwner= this.props.note.owner_id == AppUserStore.state.user_id
-
         return (
             <div className={'note-container ' + (this.props.selected ? 'selected' : '')} id={this.props.note.id} >
                 <h3>{truncate(parseNoteTitle(this.props.note.data.title), MAX_NOTE_CONTENT_PREVIEW_LENGTH)}</h3>
                 <div className="note-tags-container">
-                    {this.props.note.tags.length > 0 ? this.props.note.tags.slice(0, MAX_TAGS_PREVIEW_LENGTH).map(tag => (
+                    {this.props.note?.tags?.length > 0 ? this.props?.note?.tags.slice(0, MAX_TAGS_PREVIEW_LENGTH).map(tag => (
                         <span className="note-tag">{tag}</span>
                     )) : ""}
-                    {this.props.note.tags.length > MAX_TAGS_PREVIEW_LENGTH ? <span className="note-tag">+{(this.props.note.tags.length - MAX_TAGS_PREVIEW_LENGTH).toString()}</span> : ""}
+                    {/*{this.props.note?.tags?.length > MAX_TAGS_PREVIEW_LENGTH ? <span className="note-tag">+{(this.props.note?.tags?.length - MAX_TAGS_PREVIEW_LENGTH).toString()}</span> : ""}*/}
                 </div>
                 <span className="update-time">{formatDate(this.props.note.update_time)}</span>
             </div>
