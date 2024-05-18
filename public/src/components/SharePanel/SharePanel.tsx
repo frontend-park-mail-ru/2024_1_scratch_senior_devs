12af1b72b94fdc3951a3312f38bc0e700459a8d9
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-05-18 16:15:54 +0300

    Сделал экспорт ссылки на заметку в соц сети

--- public/src/components/SharePanel/SharePanel.tsx
+++ public/src/components/SharePanel/SharePanel.tsx
@@ -3,8 +3,34 @@ import {Button} from "../Button/Button";
 import "./SharePanel.sass"
 import {ToggleButton} from "../ToggleButton/ToggleButton";
 import {Img} from "../Image/Image";
+import {AppNotesStore} from "../../modules/stores/NotesStore";
+import {parseNoteTitle} from "../../modules/utils";
 
 export class SharePanel extends ScReact.Component<any, any> {
+
+    shareToVK = () => {
+        const url = "https://vk.com/share.php?url=" + window.location.href + "&title=" + parseNoteTitle(AppNotesStore.state.selectedNote.title)
+        this.openShareWindow(url)
+    }
+
+    shareToOdnoklassniki = () => {
+        const url = "https://connect.ok.ru/offer?url=" + window.location.href + "&title=" + parseNoteTitle(AppNotesStore.state.selectedNote.title)
+        this.openShareWindow(url)
+    }
+
+    shareToWhatsApp = () => {
+        const url = "https://wa.me/?text=" + window.location.href
+        this.openShareWindow(url)
+    }
+
+    openShareWindow = (url:string) => {
+        const width = 800, height = 500;
+        const left = (window.screen.width - width) / 2;
+        const top = (window.screen.height - height) / 2;
+        const social_window = window.open(url, "share_window", "height=" + height + ",width=" + width + ",top=" + top + ",left=" + left);
+        social_window.focus();
+    }
+
     render() {
         return (
             <div className="share_panel">
@@ -27,10 +53,9 @@ export class SharePanel extends ScReact.Component<any, any> {
                 <div className="share_panel__social-btns-container">
                     <h3>Поделиться через соц. сети</h3>
                     <div className="share_panel__social-btns-container__bottom-container">
-                        <Img src="vk.png" className="social-icon"/>
-                        <Img src="odnoklassniki.png" className="social-icon" />
-                        <Img src="whatsapp.png" className="social-icon" />
-                        <Img src="viber.png" className="social-icon" />
+                        <Img src="vk.png" className="social-icon" onClick={this.shareToVK}/>
+                        <Img src="odnoklassniki.png" className="social-icon" onClick={this.shareToOdnoklassniki} />
+                        <Img src="whatsapp.png" className="social-icon" onClick={this.shareToWhatsApp} />
                     </div>
                 </div>
             </div>
