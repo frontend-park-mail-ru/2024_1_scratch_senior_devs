313c22cfab5811009ca729d60554170c5fd3c6a0
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-05-23 08:28:00 +0300

    Добавил возможность открыть пошереную заметку в полноэкранном режиме

--- public/src/components/ViewerWrapper/ViewerWrapper.tsx
+++ public/src/components/ViewerWrapper/ViewerWrapper.tsx
@@ -7,18 +7,20 @@ import {AppDispatcher} from "../../modules/dispatcher";
 import {NotesActions} from "../../modules/stores/NotesStore";
 import {Editor} from "../Editor/Editor";
 import {AppRouter} from "../../modules/router";
+import {NoteType} from "../../utils/types";
 
-export class ViewerWrapper extends ScReact.Component<any, any> {
+type ViewerWrapperProps = {
+    fullScreen: boolean
+    note: NoteType
+    openFullScreen: () => void
+    closeFullScreen: () => void
+}
+
+export class ViewerWrapper extends ScReact.Component<ViewerWrapperProps, any> {
     private noteContentRef
     private viewerRef
 
-    state = {
-        fullScreen: false
-    }
-
     componentDidMount() {
-        console.log(this.props.note)
-
         this.noteContentRef.innerHTML = ""
         new Editor(
             this.props.note.data.content,
@@ -29,32 +31,17 @@ export class ViewerWrapper extends ScReact.Component<any, any> {
         );
     }
 
-    closeEditor = () => {
+    closeViewer = () => {
         AppRouter.go("/")
-    }
-
-    openFullScreen = () => {
-        AppDispatcher.dispatch(NotesActions.OPEN_FULLSCREEN)
-    }
-
-    closeFullScreen = () => {
-        AppDispatcher.dispatch(NotesActions.CLOSE_FULLSCREEN)
     }
 
     exportToPDF = () => {
         AppDispatcher.dispatch(NotesActions.EXPORT_TO_PDF, this.viewerRef.outerHTML)
     }
 
-    exportToZip = () => {
-        AppDispatcher.dispatch(NotesActions.EXPORT_TO_ZIP, {
-            note_id: this.props.note.id,
-            content: this.viewerRef.outerHTML
-        })
-    }
-
     render() {
         return (
-            <div className="note-viewer-wrapper">
+            <div className={"note-viewer-wrapper " + (this.props.fullScreen ? "fullscreen" : "")}>
 
                 {/*<div className="note-background" style={`background: ${this.props.note.header};`}>*/}
 
@@ -62,7 +49,7 @@ export class ViewerWrapper extends ScReact.Component<any, any> {
 
                 <div className="top-panel" style={`background: ${this.props.note.header};`}>
 
-                    <div className="back-btn-container" onclick={this.closeEditor}>
+                    <div className="back-btn-container" onclick={this.closeViewer}>
                         <Img src="left-chevron.svg" className="back-icon"/>
                         <span className="back-label">На главную</span>
                     </div>
@@ -74,23 +61,22 @@ export class ViewerWrapper extends ScReact.Component<any, any> {
                     <NoteMenu
                         note={this.props.note}
                         onExportToPdf={this.exportToPDF}
-                        onExportToZip={this.exportToZip}
                     />
 
-                    {!this.state.fullScreen ?
+                    {!this.props.fullScreen ?
                         <Tooltip
                             icon="full-screen-open.svg"
                             hoverTooltip="На весь экран"
                             showHoverTooltip={true}
                             className="toggle-fullscreen-btn"
-                            onClick={this.openFullScreen}/>
+                            onClick={this.props.openFullScreen}/>
                         :
                         <Tooltip
                             icon="full-screen-close.svg"
                             showHoverTooltip={true}
                             hoverTooltip="Уменьшить"
                             className="toggle-fullscreen-btn"
-                            onClick={this.closeFullScreen}/>
+                            onClick={this.props.closeFullScreen}/>
                     }
 
                 </div>
--- public/src/pages/SharedNote/index.tsx
+++ public/src/pages/SharedNote/index.tsx
@@ -4,10 +4,28 @@ import "../../components/Editor/Editor.sass"
 import {ViewerWrapper} from "../../components/ViewerWrapper/ViewerWrapper";
 
 export class SharedNotePage extends ScReact.Component<any, any> {
+    state = {
+        fullScreen: false
+    }
+
+    openFullScreen = () => {
+        this.setState(state => ({
+            ...state,
+            fullScreen: true
+        }))
+    }
+
+    closeFullScreen = () => {
+        this.setState(state => ({
+            ...state,
+            fullScreen: false
+        }))
+    }
+
     render () {
         return (
-            <div className="shared-note-page">
-                <ViewerWrapper note={this.props.note} />
+            <div className={"shared-note-page " + (this.state.fullScreen ? "fullscreen" : "")}>
+                <ViewerWrapper note={this.props.note} fullScreen={this.state.fullScreen} openFullScreen={this.openFullScreen} closeFullScreen={this.closeFullScreen}/>
             </div>
         )
     }
